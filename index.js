2805baaeb43e3199b87d1e8f041cca83beef1207
gaurav <gauraevg1234@gmail.com>
2020-08-31 23:16:15 +0530

    hashed password & implemented login funt with jsonwebtoken

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 const { User } = require('./models/user');
+const { json } = require('body-parser');
 
 //connect to database
 
@@ -24,18 +25,51 @@ app.post('/api/user/register',(req,res) => {
     const user = new User(req.body);
 
     user.save((err,userData) => {                                             //we have to save this in mangodb after saving we can get either error ir user data 
-        if(err) return res.json({ succes:false, err})                          //if error occurs
+        if(err)  
+            return res.json({ success:false, err})                          //if error occurs
 
-        return res.status(200).json({
-            success:true
-        });                //returning succes status 
+        return res.status(200).json({                       //returning succes status 
+            success:true        
+        });                
     })
 
    
 })
 
-app.get('/', (req,res) => {
-    res.send('hello world');
-});
+//function def for comparePassword,generateToken is written in user.js
+
+app.post('/api/user/login', (req,res) => {
+    //find the email
+    User.findOne({ email: req.body.email }, (err,user) => {
+        if(!user)
+            return res.json({
+                loginSuccess:false,
+                message:'Auth failed,email not found'
+            });
+        
+        //compare password
+        user.comparePassword(req.body.password , (err,isMatch) => {
+            if(!isMatch)
+                return res.json({
+                    loginSuccess:false,
+                    message:'password wrong'
+                });
+        });
+
+        //generate Token
+        user.generateToken((err,user) => {
+            if(err) return res.status(400).send(err);   //if functn called in user.js returns error then send error code & response as error
+            res.cookie('x_auth',user.token)             //if successfull then token we put token we made into cookie
+                .status(200)
+                .json({
+                    loginSuccess:true
+                })
+        });
+
+
+
+    })
+})
+   
 
 app.listen(5000);
--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;           //seting characters to encryypt
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name:{
@@ -29,6 +32,45 @@ const userSchema = mongoose.Schema({
         type:Number
     }
 })
+
+//for hashing password
+userSchema.pre('save',function(next){                         //this means before saving our info
+    var user = this;   //here this means userSchema
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds,function(err,salt){
+            if (err)
+                return next(err);
+            bcrypt.hash(user.password,salt,function(err,hash){                    //hash is generated hashing password
+                if(err)
+                  return next(err);
+                user.password=hash;
+                next();
+            });
+        });
+    } else{
+        next();
+    }
+});
+
+//cb -> callback functions -> (err,isMatch)
+userSchema.methods.comparePassword = function(plainPassword,cb){
+    bcrypt.compare(plainPassword,this.password, function(err,isMatch){
+        if(err) return cb(err);
+        cb(null,isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(),'secret')
+
+    user.token = token;
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null,user);
+    })
+}
 
 const User = mongoose.model('User',userSchema); //User is name of collection tat we want to create
 
